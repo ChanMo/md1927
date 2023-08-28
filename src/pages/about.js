14fa58a5f4929e82955fb0b08325f3f211491f61
xuhang mu <muxuhang@gmail.com>
2023-08-28 16:22:29 +0800

    联系我们

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import { Box, Container, Grid, Typography } from "@mui/material";
 import Layout from "../components/Layout";
+import { Map, Marker } from "react-amap";
 
 function AboutPage() {
   return <Layout banner={'/images/banner.jpg'} bannerContent={
@@ -17,11 +18,32 @@ function AboutPage() {
         专业、独特的产品设计理念，完全定制化研发，搞笑的开发流程，助您迈出创业第一步。
       </Typography>
     </Container>}>
-    <Box sx={{ backgroundColor: 'grey.100', py: 20, alignItems: 'center' }}>
-      <Container maxWidth="xl">
+    <Box sx={{ backgroundColor: 'grey.100', py: 10, alignItems: 'center' }}>
+      <Container>
+        <Box sx={{ mb: 10 }}>
+          <Typography sx={{ textAlign: 'center' }} variant="h3" gutterBottom>关于我们</Typography>
+          <Typography gutterBottom>济南漫点信息科技是一家专注于互联网项目定制开发的公司，成立于 2019 年。公司技术前沿，擅于从 0 开发各类创新型应用。</Typography>
+          <Typography gutterBottom>公司主营业务涵盖多个行业和领域，包括区块链、人工智能、物联网、Sass 系统、教育、医疗等。公司致力于为客户提供高质量、高性价比的项目开发服务，帮助客户实现业务目标。</Typography>
+          <Typography gutterBottom>公司拥有一支经验丰富的技术团队，成员具有丰富的项目开发经验，擅长使用各种前沿技术。</Typography>
+          <Typography gutterBottom>公司拥有完善的项目管理流程，确保项目按时、高质量完成。公司还提供售后支持，帮助客户解决项目使用过程中遇到的问题。</Typography>
+
+        </Box>
+      </Container>
+      <Container>
         <Grid container spacing={3}>
           <Grid item sm={12} md={5} sx={{ 'img': { width: '100%' } }}>
-            <img src="/images/banner.jpg" alt="" />
+            <Box sx={{ height: 320 }}>
+              <Map
+                amapkey="0f969a22ab41e6bf775642a51aabdc6f"
+                plugins={{
+
+                }}
+                center={{longitude: 117.109517, latitude: 36.695167}}
+                zoom={14}
+              >
+                <Marker position={{ longitude: 117.109517, latitude: 36.695167 }} />
+              </Map>
+            </Box>
           </Grid>
           <Grid item sm={12} md={6}>
             <Box sx={{
@@ -32,13 +54,11 @@ function AboutPage() {
               height: '100%',
               justifyContent: 'center'
             }}>
-              <Typography sx={{ mb: 5 }} variant="h2" gutterBottom>Contact Us</Typography>
-              <Typography gutterBottom>
-                Company Address: 山东省济南市花园东路88号
-              </Typography>
-              <Typography gutterBottom>Email: chan.mo@outlook.com</Typography>
-              <Typography gutterBottom>Phone: 18743238391</Typography>
-              <Typography gutterBottom>Wechat: chanmo1927</Typography>
+              <Typography sx={{ mb: 5 }} variant="h3" gutterBottom>联系我们</Typography>
+              <Typography gutterBottom>地址: 山东省济南市花园东路88号</Typography>
+              <Typography gutterBottom>邮箱: chan.mo@outlook.com</Typography>
+              <Typography gutterBottom>手机: 18743238391</Typography>
+              <Typography gutterBottom>微信: chanmo1927</Typography>
             </Box>
           </Grid>
         </Grid>
--- src/pages/case.js
+++ src/pages/case.js
@@ -1,8 +1,75 @@
+import { Box, Button, Card, Container, Grid, Typography } from "@mui/material";
 import Layout from "../components/Layout";
 
 function CasePage() {
-  return <Layout banner={'/images/banner.jpg'}>
+  const list = [
+    {
+      title: '户外运动-运动跑图',
+      type: '小程序',
+      createAt: '2021年3月13日',
+      image: '/images/banner.jpg',
+      introduction: '户外运动是一款运动跑图的产品，用户购买线路后通过记录步数完成'
+    },
+    {
+      title: '户外运动-运动跑图',
+      type: '小程序',
+      createAt: '2021年3月13日',
+      image: '/images/banner.jpg',
+      introduction: '户外运动是一款运动跑图的产品，用户购买线路后通过记录步数完成'
+    }
+  ]
+  return <Layout banner={'/images/banner.jpg'} bannerContent={
+    <Container maxWidth="xl" sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100%'
+    }}>
+      <Typography variant="h2" gutterBottom color="white">
+        专注于创新应用开发
+      </Typography>
+      <Typography variant="h5" color="white">
+        科技创新，引领未来
+      </Typography>
+    </Container>}>
 
+    <Box sx={{ background: "#fff" }}>
+      <Container sx={{ py: 5 }}>
+        {list.map((item, index) => {
+          return <Card key={index}
+            sx={{ mb: 3, boxShadow: '1px 5px 10px 0 #f3f3f3' }}>
+            <Grid container>
+              <Grid item xs={12} md={4}>
+                <img style={{ width: '100%', display: 'block' }} src={item.image} alt='' />
+              </Grid>
+              <Grid item xs={12} md={8} sx={{
+                p: 3, display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between'
+              }}>
+                <Box>
+                  <Typography variant="h4" gutterBottom>{item.title}</Typography>
+                  <Typography>
+                    {item.type}
+                  </Typography>
+                </Box>
+                <Box>
+                  <Button variant="outlined">查看详情</Button>
+                </Box>
+              </Grid>
+            </Grid>
+          </Card>
+        })}
+        <Box sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}>
+          <div>加载更多</div>
+        </Box>
+      </Container>
+    </Box>
   </Layout>
 }
 
