9ac0d2702984d588354f53019ee764089e50bd3c
xuhang mu <muxuhang@gmail.com>
2023-09-06 15:24:50 +0800

    zg 文案初版

--- src/pages/zhigao.js
+++ src/pages/zhigao.js
@@ -30,9 +30,9 @@ function ZhiGaoPage() {
       },
     };
     return <Box sx={{
-      '.swiper':{
-        paddingBottom:'24px',
-        '.swiper-pagination-bullet':{
+      '.swiper': {
+        paddingBottom: '24px',
+        '.swiper-pagination-bullet': {
           // width:30,
           // height:16,
           // color:"#fff",
@@ -56,7 +56,7 @@ function ZhiGaoPage() {
       </Swiper>
     </Box>
   }
-  const client_list = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
+  const client_list = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
   return (
     <Layout
       // banner={'/images/banner.jpg'}
@@ -145,21 +145,18 @@ function ZhiGaoPage() {
                 </div>
               </Grid>
               <Grid item xs={12} md={6} sx={{
-                pr: 10,
                 '@media screen and (max-width:900px)': {
                   pr: 0
                 },
               }}>
                 <Typography variant='h4'
-                  sx={{
-                    my: 4,
-                    '@media screen and (max-width:900px)': {
-                      my: 2,
-                    },
-                  }}>专为中职高考设计</Typography>
-                <Typography gutterBottom>
+                  sx={{ mt: 2 }} gutterBottom>专为中职高考设计</Typography>
+                <Typography sx={{ mb: 1 }} gutterBottom>
                   平台涵盖几十种中职院校专业课程<br />
                   由从业多年的教师亲自设计的考题
+                </Typography>
+                <Typography sx={{ fontSize: '.875rem', color: "#666" }}>
+                  职业高中学校培养与中国社会主义现代化建设要求相适应，德、智、体、美全面发展，具有综合职业能力，在生产、服务一线工作的高素质劳动者和技能型人才 。他们应当热爱社会主义祖国，能够将实现自身价值与服务祖国人民结合起来；具有基本的科学文化素养、继续学习的能力和创新精神；具有良好的职业道德，掌握必要的文化基础知识、专业知识和比较熟练的职业技能，具有较强的就业能力和一定的创业能力；具有健康的身体和心理；具有基本的欣赏美和创造美的能力。
                 </Typography>
               </Grid>
               <Grid item xs={12} md={6} sx={{
@@ -168,7 +165,7 @@ function ZhiGaoPage() {
                 }
               }}>
                 <div className='about_img'>
-                  <img src="/images/banner.jpg" alt=''></img>
+                  <img src="/images/zhigao/zg_b3.jpg" alt=''></img>
                 </div>
               </Grid>
             </Grid>
@@ -193,25 +190,28 @@ function ZhiGaoPage() {
             <Grid container rowSpacing={0} columnSpacing={20}>
               <Grid item xs={12} md={6}>
                 <div className='about_img'>
-                  <img src="/images/banner.jpg" alt=''></img>
+                  <img src="/images/zhigao/zg_b2.jpg" alt=''></img>
                 </div>
               </Grid>
               <Grid item xs={12} md={6}>
                 <Typography variant='h4'
                   sx={{
-                    my: 4,
-                    '@media screen and (max-width:900px)': {
-                      my: 2,
-                    },
-                  }}>在线视频课程</Typography>
-                <Typography gutterBottom>
-                  测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试
+                    mt: 2,
+                  }} gutterBottom>在线视频课程</Typography>
+                <Typography sx={{ fontSize: '.875rem', color: "#666" }} gutterBottom>
+                  学习时间可控，可以随意调配，学习地点更加灵活方便。对于学生来说没有什么软硬件的要求，只需要有一部手机随时随地就可以上课学习了。
+                </Typography>
+                <Typography sx={{ fontSize: '.875rem', color: "#666" }} gutterBottom>
+                  在线视频课程的具有任何人，任何时间，任何地点，从任何章节开始，学习任何课程等特点。这些特点决定不用担心学习资料的同步问题，不用担心时间的延误问题，只要有网就能及时开始学习，所有资源都在云服务器上，内容也会不断更新，确保学生学习的及时和快速性。
+                </Typography>
+                <Typography sx={{ fontSize: '.875rem', color: "#666" }} gutterBottom>
+                  在线视频课程可以反复学习，学生可以根据自己学习掌握的情况，重听或者重学部分学习内容，更好的掌握所要学习的内容，避免学过就忘的问题，巩固效果。
                 </Typography>
               </Grid>
             </Grid>
           </Box>
         </Container>
-      </Box>
+      </Box >
       <Box style={{ background: "#fff" }}>
         <Container sx={{
           py: 5,
@@ -234,25 +234,23 @@ function ZhiGaoPage() {
                 },
               }}>
                 <div className='about_img'>
-                  <img src="/images/banner.jpg" alt=''></img>
+                  <img src="/images/zhigao/zg_b4.jpg" alt=''></img>
                 </div>
               </Grid>
               <Grid item xs={12} md={6} sx={{
-                pr: 10,
                 '@media screen and (max-width:900px)': {
                   pr: 0
                 },
               }}>
                 <Typography variant='h4'
                   sx={{
-                    my: 4,
-                    '@media screen and (max-width:900px)': {
-                      my: 2,
-                    },
-                  }}>在线考试系统</Typography>
-                <Typography gutterBottom>
-                  平台涵盖几十种中职院校专业课程<br />
-                  由从业多年的教师亲自设计的考题
+                    mt: 2
+                  }} gutterBottom>在线考试系统</Typography>
+                <Typography sx={{ fontSize: '.875rem', color: "#666" }} gutterBottom>快捷方便、省时省力，打破时间和地域的限制，随时随地可以进行。</Typography>
+                <Typography sx={{ fontSize: '.875rem', color: "#666" }} gutterBottom>成本低，效率高，借用当今互联网优势，通过考试系统发布考试，将二维码和考试链接散发给考生即可完成组织考试。 </Typography>
+                <Typography sx={{ fontSize: '.875rem', color: "#666" }} gutterBottom>不需要像传统考试那样，人工阅卷，花费大量人力物力，效率还低，且容易出错，在线考试系统都是自动判卷出成绩。</Typography>
+                <Typography sx={{ fontSize: '.875rem', color: "#666" }} gutterBottom>
+                  精准的考试报表分析（如考试分析、成绩分析、考生分析、错题分析等），不再像传统考试需要人工经过大量统计工作才能得到一份报表，大大节省时间和成本。
                 </Typography>
               </Grid>
               <Grid item xs={12} md={6} sx={{
@@ -261,7 +259,7 @@ function ZhiGaoPage() {
                 },
               }}>
                 <div className='about_img'>
-                  <img src="/images/banner.jpg" alt=''></img>
+                  <img src="/images/zhigao/zg_b4.jpg" alt=''></img>
                 </div>
               </Grid>
             </Grid>
@@ -274,7 +272,7 @@ function ZhiGaoPage() {
           '@media screen and (max-width:900px)': {
             pb: 0
           }
-        }}>
+        }} maxWidth='md'>
           <Typography variant='h4'
             sx={{
               mb: 4, textAlign: 'center',
